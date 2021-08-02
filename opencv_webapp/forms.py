c8058e9f4bc0c70eda249a71df3b93fd0c2adbb1
Cottonwood <blog_geon@naver.com>
2021-08-02 10:07:24 +0900

    First commit

---
+++ opencv_webapp/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from .models import ImageUploadModel
+# class PostForm(forms.ModelForm):
+#     class Meta:
+#         model = Model 클래시 이름(@ models.py)
+#         field = ['Model 클래스의 변수명 1','Model 클래스의 변수명 2']
+#
+
+class SimpleUploadForm(forms.Form):
+    title = forms.CharField(max_length=50)
+    # ImageField Inherits all attributes and methods from FileField, but also validates that the uploaded object is a valid image.
+    # file = forms.FileField()
+    image = forms.ImageField()
+
+class ImageUploadForm(forms.ModelForm):
+
+    class Meta:
+        model = ImageUploadModel
+        fields = ('description', 'document')
